944fad71d5e3ad72ef78923eb3c772d5a5979e6c
Viktor <vsirotkin15@gmail.com>
2022-02-28 13:27:26 +0300

    user class setup
    email - unique

--- usersapp/models.py
+++ usersapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64, unique=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(empty_value=False, unique=True)
+    
+    def _validate_email(email):
+        try:
+            validate_email(email)
+            return True
+        except ValidationError:
+            return False
